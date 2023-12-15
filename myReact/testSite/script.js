2a261341d464d9c3e87c55249e17a609ac624be2
manhellis <manh@manhellis.com>
2023-12-15 14:01:52 -0800

    display home json file

--- myReact/testSite/script.js
+++ myReact/testSite/script.js
@@ -1,47 +1,33 @@
-$(document).ready(function() {
-    // Load the JSON file
-    $.getJSON('seasons-35.json', function(data) {
-        var table = $('#myTable tbody');
-        var yearSet = new Set(); // To store unique years
+const table = $("#myTable tbody");
 
-        // Process each item in the JSON array
-        $.each(data, function(key, item) {
-            // Extract year from 'starts_at' property
-            var year = new Date(item.starts_at).getFullYear();
-            yearSet.add(year);
+// Function to load JSON file and append it to the table
+const loadAndAppendJSON = () => {
+    const jsonFileUrl = "./api-v1/seasons-35.json";
 
-            // Add rows to the table
-            var row = $('<tr></tr>');
-            row.append($('<td></td>').text(item.event));
-            row.append($('<td></td>').text(item.event_id));
-            table.append(row);
-        });
-
-        // Populate year filter dropdown
-        var yearFilter = $('#filter1');
-        yearSet.forEach(function(year) {
-            yearFilter.append($('<option></option>').val(year).text(year));
-        });
-    }).fail(function() {
-        console.error("Error loading JSON data");
-    });
-});
-
-// Filter function
-function filterTable() {
-    var selectedYear = document.getElementById("filter1").value;
-    var table = document.getElementById("myTable");
-    var tr = table.getElementsByTagName("tr");
-
-    for (var i = 1; i < tr.length; i++) {
-        var td = tr[i].getElementsByTagName("td")[0]; // Assuming the date is in the first column
-        if (td) {
-            var year = new Date(td.textContent || td.innerText).getFullYear().toString();
-            if (year === selectedYear || selectedYear === "") {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
+    fetch(jsonFileUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            // Create header row
+            let headerRow = $("<tr></tr>");
+            if (data.events && data.events.length > 0) {
+                Object.keys(data.events[0]).forEach((key) => {
+                    headerRow.append($(`<th>${key}</th>`));
+                });
+                table.append(headerRow);
             }
-        }
-    }
-}
+            // Create and append data rows
+            data.events.forEach((event) => {
+                let row = $("<tr></tr>");
+                Object.values(event).forEach((value) => {
+                    if (typeof value === "object" && value !== null) {
+                        value = JSON.stringify(value);
+                    }
+                    row.append($(`<td>${value}</td>`));
+                });
+                table.append(row);
+            });
+        })
+        .catch((error) => console.error("Error loading JSON file:", error));
+};
+// Call the function to load and append the JSON file
+loadAndAppendJSON();
