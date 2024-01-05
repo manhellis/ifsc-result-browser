00e12a1d207df129f0ea06e29274a8f7908f3d80
manhellis <manh@manhellis.com>
2024-01-05 14:55:37 -0800

    single event displaying support

---
+++ myReact/ifsc-result-browser/src/app/components/eventPage.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const EventPageHeader = ({ event }) => {
+    const startDate = new Date(event.starts_at).toLocaleString("en-US", {
+        dateStyle: "full",
+        timeStyle: "short",
+        timeZone: "UTC",
+    });
+
+    return (
+        <header className="bg-gray-200 text-gray-700 p-4">
+            <h1 className="text-3xl font-bold">{event.name}</h1>
+            <p>ID: {event.id}</p>
+            <p>Location: {event.location}</p>
+            <p>Starts at: {startDate}</p>
+        </header>
+    );
+};
+
+const CategoryRounds = ({ category }) => {
+    // pass in dcat
+    const rounds = category.category_rounds.map((round) => {
+        return (
+            <div key={round.category}>
+                <h2>
+                    {`${round.name} ${round.category} ${round.format} `}
+
+                    <span>{`${round.result_url}`}</span>
+                </h2>
+
+                <p>{round.round_date}</p>
+            </div>
+        );
+    });
+
+    return (
+        <div>
+            {/* <h2>Rounds</h2> */}
+            {rounds}
+        </div>
+    );
+};
+
+const EventPageCategory = ({ event }) => {
+    return event.d_cats.map((category) => {
+        // Added return statement here
+        // console.log(category.category_name);
+        return (
+            <div key={category.d_cat_id}>
+                <h1 className="text-xl">{`${category.category_name} ${category.discipline_kind}`}</h1>
+                <CategoryRounds category={category} />
+            </div>
+        );
+    });
+};
+
+const EventPage = ({ event }) => {
+    // Your component logic here
+    // console.log(event)
+    return (
+        // Your JSX code here
+        <div>
+            <EventPageHeader event={event} />
+            <EventPageCategory event={event} />
+        </div>
+    );
+};
+
+export default EventPage;
--- myReact/ifsc-result-browser/src/app/page3/page.js
+++ myReact/ifsc-result-browser/src/app/page3/page.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import singleEvent from '../../data/singleEvent.json';
-import EventPageHeader from '../components/eventPageHeader';
-import EventPageContent from '../components/eventPageContent';
+import EventPage from '../components/eventPage'
 
 
 const Page = () => {
-
+    // console.log(singleEvent.name)
     
     // console.log(singleEvent.d_cats);
     return (
         <div className='flex flex-col justify-center items-center'>
-            <EventPageHeader eventPage={singleEvent} />
-            {singleEvent.d_cats.map((cat, index) => (
-                <EventPageContent key={index} category={cat} />
-            ))}
+            <EventPage event={singleEvent} />
+            
         </div>
     );
 };
